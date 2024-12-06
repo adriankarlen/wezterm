65e56f595827ee21924f3bfaaf42c9d919c5bcd6
Adrian Karlén <adrian.karlen@spp.se>
2024-12-06 15:31:46 +0100

    fix: use correct main config file

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,10 @@
 local wez = require "wezterm"
 local utils = require "lua.utils"
-
 local appearance = require "lua.appearance"
-local bar = wez.plugin.require "https://github.com/adriankarlen/bar.wezterm"
 local mappings = require "lua.mappings"
+local bar = wez.plugin.require "https://github.com/adriankarlen/bar.wezterm"
 
+local platform = utils.platform()
 local c = {}
 
 if wez.config_builder then
@@ -16,17 +16,18 @@ c.font = wez.font("SF Mono", { weight = "Medium" })
 c.font_rules = {
   {
     italic = true,
-    intensity = "Half",
     font = wez.font("SF Mono", { weight = "Medium", italic = true }),
   },
 }
-c.font_size = 12
-c.default_prog = utils.is_windows and { "pwsh", "-NoLogo" } or "zsh"
+c.font_size = 13
+c.command_palette_font_size = 13
+c.command_palette_rows = 15
+c.default_prog = platform.is_win and { "pwsh", "-NoLogo" } or "zsh"
 c.adjust_window_size_when_changing_font_size = false
 c.audible_bell = "Disabled"
 c.scrollback_lines = 3000
 c.default_workspace = "main"
-c.status_update_interval = 2000
+c.max_fps = 240
 
 -- appearance
 appearance.apply_to_config(c)
@@ -35,6 +36,18 @@ appearance.apply_to_config(c)
 mappings.apply_to_config(c)
 
 -- bar
-bar.apply_to_config(c, { enabled_modules = { hostname = false } })
+bar.apply_to_config(c, {
+  modules = {
+    clock = {
+      enabled = false,
+    },
+    username = {
+      icon = "",
+    },
+    spotify = {
+      enabled = true,
+    },
+  },
+})
 
 return c
